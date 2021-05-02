7c9ef3daeb74eccabb01b2bac4708846570685a3
GeonNamGung <rndrjs123@naver.com>
2021-05-03 02:15:20 +0900

    "필터 설정" page is connected with "유치원 검색"
    Added startActivity(유치원검색) event to button

--- app/src/main/java/com/example/hci_project/SearchSchoolActivity.kt
+++ app/src/main/java/com/example/hci_project/SearchSchoolActivity.kt
@@ -1,11 +1,15 @@
 package com.example.hci_project
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.hci_project.databinding.ActivitySearchSchoolBinding
 
 class SearchSchoolActivity : AppCompatActivity() {
     lateinit var binding:ActivitySearchSchoolBinding
+    val FILTER_SET_ACTIVITY= 1234
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +18,24 @@ class SearchSchoolActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.title= "유치원 검색"
+
+        initListener()
+    }
+
+    fun initListener(){
+        binding.apply {
+            filterBtn.setOnClickListener{
+                val intent= Intent(this@SearchSchoolActivity, SearchFilterActivity::class.java)
+                startActivityForResult(intent, FILTER_SET_ACTIVITY)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //handle filterInfo
+        if(requestCode == FILTER_SET_ACTIVITY && resultCode == Activity.RESULT_OK){
+            Toast.makeText(this, "필터가 적용되었습니다", Toast.LENGTH_SHORT).show()
+        }
     }
 }
