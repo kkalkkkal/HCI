0e1e5325f9f8981451a1094aab9ad90528d38d49
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-05-05 22:17:38 +0900

    IntroActivity -> MainActivity 전환 코드 추가

--- app/src/main/java/com/example/hci_project/IntroActivity.java
+++ app/src/main/java/com/example/hci_project/IntroActivity.java
@@ -2,6 +2,7 @@ package com.example.hci_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.naver.maps.map.NaverMapSdk;
@@ -15,5 +16,15 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_page);
 
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Intent mainintent = new Intent(IntroActivity.this, MainActivity.class);
+        mainintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//뒤로가기 버튼 눌렀을때 메인에서 로그인으로 못가게?
+        IntroActivity.this.startActivity(mainintent); // 메인 액티비티로 이동
+        finish();
     }
 }
