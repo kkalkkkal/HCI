b75a51a78b0d8a93cd0e5f9e0336ce672e9e06be
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-05-05 20:32:36 +0900

    네이버 맵 뷰 추가

--- app/src/main/java/com/example/hci_project/MainActivity.java
+++ app/src/main/java/com/example/hci_project/MainActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.naver.maps.map.NaverMapSdk;
+
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_page);
+        try {
+            sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
+        NaverMapSdk.getInstance(this).setClient(
+                new NaverMapSdk.NaverCloudPlatformClient("o82z0vth6u"));
+
         //
     }
 }
