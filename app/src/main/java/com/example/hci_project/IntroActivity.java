9ccd7c6ac4fce534a1f7c2e91e9040a2ff75f951
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-05-05 23:43:36 +0900

    인트로페이지를 스플래시 화면으로 전환중 1

--- app/src/main/java/com/example/hci_project/IntroActivity.java
+++ app/src/main/java/com/example/hci_project/IntroActivity.java
@@ -13,13 +13,10 @@ public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.intro_page);
+        super.onCreate(savedInstanceState); // 스플래시 화면 생성
 
-
-        Intent mainintent = new Intent(IntroActivity.this, MainActivity.class);
-        mainintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//뒤로가기 버튼 눌렀을때 메인에서 로그인으로 못가게?
-        IntroActivity.this.startActivity(mainintent); // 메인 액티비티로 이동
+        Intent mainintent = new Intent(this, MainActivity.class);
+        startActivity(mainintent); // 메인 액티비티로 이동
         finish();
     }
 }
