0f2cd33d2ba38d88a4928c85c9477d6319983389
NamGungGeon <rndrjs123@naver.com>
2021-05-12 01:27:18 +0900

    "유치원 정보" page is connected with "유치원 검색", "즐겨찾기"

--- app/src/main/java/com/example/hci_project/SearchResultFragment.kt
+++ app/src/main/java/com/example/hci_project/SearchResultFragment.kt
@@ -2,7 +2,6 @@ package com.example.hci_project
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
@@ -10,50 +9,21 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.hci_project.dummy.DummySchoolContent
 
-/**
- * A fragment representing a list of Items.
- */
 class SearchResultFragment : Fragment() {
 
-    private var columnCount = 1
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_search_result, container, false)
 
         // Set the adapter
         if (view is RecyclerView) {
             with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
+                layoutManager = LinearLayoutManager(context)
                 adapter = SearchResultRecyclerViewAdapter(DummySchoolContent.ITEMS)
             }
         }
         return view
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-                SearchResultFragment().apply {
-                    arguments = Bundle().apply {
-                        putInt(ARG_COLUMN_COUNT, columnCount)
-                    }
-                }
     }
 }
--- app/src/main/java/com/example/hci_project/SearchResultRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/hci_project/SearchResultRecyclerViewAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.hci_project
 
+import android.content.Context
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,23 +10,26 @@ import android.widget.TextView
 
 import com.example.hci_project.dummy.DummySchoolContent.DummyItem
 
-/**
- * [RecyclerView.Adapter] that can display a [DummyItem].
- * TODO: Replace the implementation with code for your data type.
- */
 class SearchResultRecyclerViewAdapter(
         private val values: List<DummyItem>)
     : RecyclerView.Adapter<SearchResultRecyclerViewAdapter.ViewHolder>() {
 
+    private lateinit var context: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.list_search_result, parent, false)
+        this.context= parent.context
+
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
         holder.titleView.text = item.content
+        holder.itemView.setOnClickListener {
+            context.startActivity(Intent(context, SchoolInfoActivity::class.java))
+        }
     }
 
     override fun getItemCount(): Int = values.size
