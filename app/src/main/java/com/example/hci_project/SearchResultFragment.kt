261724e804d602f932251cb7a63f223a1b4e8259
NamGungGeon <rndrjs123@naver.com>
2021-06-09 02:59:58 +0900

    fix bug in SearchResultFragment

--- app/src/main/java/com/example/hci_project/SearchResultFragment.kt
+++ app/src/main/java/com/example/hci_project/SearchResultFragment.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hci_project.bean.School
+import com.example.hci_project.bean.SchoolManager
 import com.example.hci_project.bean.SearchResult
 import com.example.hci_project.bean.SearchResultManager
 
@@ -17,8 +19,8 @@ class SearchResultFragment : Fragment() {
     lateinit var msgView: TextView
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_search_result, container, false)
     }
@@ -35,19 +37,30 @@ class SearchResultFragment : Fragment() {
                     setList(ArrayList(manager.searchResultList), {
                         when (it.type) {
                             SearchResult.TYPE_SEARCH -> (activity!! as SearchSchoolActivity).search(
-                                    it.title
+                                it.title
                             )
                             SearchResult.TYPE_SCHOOL -> {
-                                SearchResultManager.getInstance().use(context!!) { manager ->
-                                    manager?.apply {
-                                        searchResultList.add(0, it)
-                                        save(context!!)
-                                    }
+                                manager?.apply {
+                                    searchResultList.add(0, it)
+                                    save(context!!)
                                 }
                                 val intent =
-                                        Intent(context, SchoolInfoActivity::class.java)
-                                intent.putExtra("school", it)
-                                context!!.startActivity(intent)
+                                    Intent(context, SchoolInfoActivity::class.java)
+                                //is attached?
+                                if (it.obj != null) {
+                                    //yes, attached!
+                                    intent.putExtra("school", it.obj as School)
+                                    context!!.startActivity(intent)
+                                } else {
+                                    //not attached. let's find.
+                                    SchoolManager.getInstance().use(context!!) { schoolManager ->
+                                        val school = schoolManager!!.list.find { target ->
+                                            target.name == it.title
+                                        }
+                                        intent.putExtra("school", school)
+                                        context!!.startActivity(intent)
+                                    }
+                                }
                             }
                         }
                     }) { currentResult ->
@@ -63,7 +76,11 @@ class SearchResultFragment : Fragment() {
         }
     }
 
-    fun setList(resultList: ArrayList<SearchResult>, onClick: ((SearchResult) -> Unit)? = null, onRemove: ((SearchResult) -> Unit)? = null) {
+    fun setList(
+        resultList: ArrayList<SearchResult>,
+        onClick: ((SearchResult) -> Unit)? = null,
+        onRemove: ((SearchResult) -> Unit)? = null
+    ) {
         msgView.text = "${resultList.size}개의 검색 결과가 있습니다"
 
         val recylcerView = view?.findViewById<RecyclerView>(R.id.search_result_list)
