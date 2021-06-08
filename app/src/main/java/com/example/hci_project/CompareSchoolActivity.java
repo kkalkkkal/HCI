a6907c38a5f0bfd5527931736a650e271857e490
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2021-06-08 17:22:02 +0900

    비교페이지 픽스

--- app/src/main/java/com/example/hci_project/CompareSchoolActivity.java
+++ app/src/main/java/com/example/hci_project/CompareSchoolActivity.java
@@ -27,6 +27,7 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.google.android.gms.common.server.converter.StringToIntConverter;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -42,11 +43,13 @@ public class CompareSchoolActivity extends AppCompatActivity {
 
     School school_data;
     public String[] adress = new String[400];
-    public int last = 5;
+    public int last = 0;
     Intent intent;
     char check = 'a';
     public int[] child_num = new int[1000];
     public int[] teacher_num = new int[1000];
+    Workbook workbook2;
+    Sheet sheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,54 +59,53 @@ public class CompareSchoolActivity extends AppCompatActivity {
         final LinearLayout lm = (LinearLayout) findViewById(R.id.ll);
         final LinearLayout lm2 = (LinearLayout) findViewById(R.id.ll2);
 
-        //comeonDB(); //DB 불러오기
-        intent = getIntent();
-       // String check = intent.getExtras().getString("last");
-        check = intent.getCharExtra("last", 'a');
-      //  school_data = (School)intent.getSerializableExtra("school"); /*클래스*/
 
-        if(check=='b')
-        {
-            last=last+1;
-         //   Toast.makeText(getApplicationContext(), "last=last+1" , Toast.LENGTH_LONG).show();
-        }else if(check=='c'){
-            last=last-1;
-        //    Toast.makeText(getApplicationContext(), "last=last-1" , Toast.LENGTH_LONG).show();
-        }else{
-        //    Toast.makeText(getApplicationContext(), "else check = "+check , Toast.LENGTH_LONG).show();
+        //intent = getIntent();
+        //check = intent.getCharExtra("last", 'a');
+
+        // UserDB.xls 불러오기
+        try {
+            File dir = new File(getApplicationContext().getFilesDir().toString());
+            File f = new File(dir, "UserDB.xls");
+
+            if (!f.exists()) {
+                f.createNewFile();
+                workbook2 = Workbook.getWorkbook(getApplicationContext().getAssets().open("UserDB.xls"));
+                //throw new Exception("file not found");
+            } else {
+                f = new File(String.valueOf(getApplicationContext().getFileStreamPath("UserDB.xls")));
+
+                    workbook2 = Workbook.getWorkbook(f);
+
+            }
+
+            if (!f.canRead()) {
+                throw new Exception("can't read file");
+            }
+
+            sheet = workbook2.getSheet(0);
+
+            last = sheet.getRows();
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-
-
-
-
-
         // linearLayout params 정의
-
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-
                 LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-
 
         // LinearLayout 생성
         LinearLayout ll = new LinearLayout(this);
-
         ll.setOrientation(LinearLayout.HORIZONTAL);
 
 
-        for (int j = 0; j <= last; j++) {
-
-/*
-            // TextView 생성
-            TextView tvProdc = new TextView(this);
-            tvProdc.setText("Name" + j + " ");
-            ll.addView(tvProdc);
-
-            // TextView 생성
-            TextView tvAge = new TextView(this);
-            tvAge.setText("   Age" + j + "  ");
-            ll.addView(tvAge);
-*/
+        for (int j = 1; j <= last; j++) {
 
             // 버튼 생성
 
@@ -111,22 +113,10 @@ public class CompareSchoolActivity extends AppCompatActivity {
 
             // setId 버튼에 대한 키값
 
-            btn.setId(j + 1);
+            btn.setId(j);
 
             if (j != last) {
-                btn.setText("유치원" + (j+1));
-                if(j==0){
-                    adress[j] = "05100";
-                }else if(j==1){
-                    adress[j] = "05015";
-                }else if(j==2){
-                    adress[j] = "04957";
-                }else if(j==3){
-                    adress[j] = "05112";
-                }else if(j==4){
-                    adress[j] = "04956";
-                }
-
+                btn.setText(sheet.getCell(1,j).getContents());
             }
             else {
                 btn.setText("+");
@@ -167,36 +157,22 @@ public class CompareSchoolActivity extends AppCompatActivity {
             int finalJ = j;
             btn.setOnClickListener(new OnClickListener() {
 
+                // Todo : 고쳐야함.
                 public void onClick(View v) {
 
                     Log.d("log", "position :" + position);
                     if (finalJ != last) {
-                        Toast.makeText(getApplicationContext(), "클릭한 유치원번호 : " + position + " 우편번호 : " + adress[position] , Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "클릭한 유치원번호 : " + position , Toast.LENGTH_LONG).show();
 
 
                     } else {
-                        last=last+1;
-                        Intent intent2 = new Intent(CompareSchoolActivity.this, CompareSchoolActivity.class);
-                        intent2.putExtra("last",last);
-                        startActivity(intent2);
 
-                        Intent intent = new Intent(CompareSchoolActivity.this, SearchSchoolActivity.class);
-                        startActivity(intent);
-                        finish();
                     }
 
 
                 }
 
             });
-
-
-            //버튼 add
-
-            //  ll.addView(btn);
-
-            //LinearLayout 정의된거 add
-
 
         }
         lm.addView(ll);
@@ -206,55 +182,47 @@ public class CompareSchoolActivity extends AppCompatActivity {
 
         PieChart pieChart = findViewById(R.id.piechart);
         ArrayList<PieEntry> NoOfEmp = new ArrayList<>();
-        for(int i = 0 ; i < last ; i++)
+        for(int i = 1 ; i < last ; i++)
         {
-            NoOfEmp.add(new PieEntry(teacher_num[i], i));
+            NoOfEmp.add(new PieEntry(Integer.parseInt(sheet.getCell(2,i).getContents()), i));
         }
 
-        PieDataSet dataSet = new PieDataSet(NoOfEmp, "총 교사수");
-        ArrayList year = new ArrayList();
-        year.add("2008");
-        year.add("2009");
-        year.add("2010");
-        year.add("2011");
-        year.add("2012");
-        year.add("2013");
-        year.add("2014");
-        year.add("2015");
-        year.add("2016");
-        year.add("2017");
+        int[] colorArray = new int[] {Color.rgb(235,119,56), Color.rgb(236,202,76),
+                Color.rgb(120,153,64), Color.rgb(171,111,72), Color.rgb(179,64,90) };
+        PieDataSet dataSet = new PieDataSet(NoOfEmp, "교사당 학생 수");
         PieData data = new PieData(dataSet);
         // MPAndroidChart v3.X 오류 발생
         pieChart.setData(data);
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        pieChart.animateXY(5000, 5000);
+        data.setValueTextSize(30);
+        pieChart.setDrawEntryLabels(true);
+        pieChart.setUsePercentValues(false);
+        data.setDrawValues(true);
+        pieChart.setCenterText("교사당 학생 수");
+        pieChart.setCenterTextSize(25);
+        dataSet.setColors(colorArray);
+        pieChart.invalidate();
 
 
         /////////////////////////////// Bar chart ///////////////////////////// childhomedb 의 col 14
 
         BarChart chart = findViewById(R.id.barchart);
         ArrayList NoOfEmp2 = new ArrayList();
-        for(int i = 0 ; i < last ; i++)
+        for(int i = 1 ; i < last ; i++)
         {
-            NoOfEmp2.add(new BarEntry(i, child_num[i]));
+            if (sheet.getCell(8,i).getContents().contains("-"))
+                NoOfEmp2.add(new BarEntry(i, 0));
+            else
+                NoOfEmp2.add(new BarEntry(i, Integer.parseInt(sheet.getCell(8,i).getContents())));
         }
 
-        ArrayList year2 = new ArrayList();
-        year2.add("2008");
-        year2.add("2009");
-        year2.add("2010");
-        year2.add("2011");
-        year2.add("2012");
-        year2.add("2013");
-        year2.add("2014");
-        year2.add("2015");
-        year2.add("2016");
-        year2.add("2017");
-        BarDataSet bardataset = new BarDataSet(NoOfEmp2, "총 유아수");
-        chart.animateY(5000);
+
+        BarDataSet bardataset = new BarDataSet(NoOfEmp2, "CCTV 개수");
         BarData data2 = new BarData(bardataset);
         // MPAndroidChart v3.X 오류 발생
-        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        bardataset.setColors(colorArray);
+
+        data2.setDrawValues(true);
+        data2.setValueTextSize(20);
         chart.setData(data2);
 
 
@@ -262,63 +230,6 @@ public class CompareSchoolActivity extends AppCompatActivity {
 
 
 
-    public void comeonDB() { // DB 불러오기
-        try {
-            InputStream is = getBaseContext().getResources().getAssets().open("kindergardenDB.xls"); // 유치원 현황
-            InputStream is2 = getBaseContext().getResources().getAssets().open("childhomeDB.xls"); // 어린이집 현황
-            Workbook wb = Workbook.getWorkbook(is);
-            Workbook wb2 = Workbook.getWorkbook(is2);
-
-            if(wb != null) {
-                Sheet sheet = wb.getSheet(0);   // 시트 불러오기
-                if(sheet != null) {
-                    int colTotal = sheet.getColumns();    // 전체 컬럼
-                    int rowIndexStart = 1;                  // row 인덱스 시작
-                    int rowTotal = sheet.getColumn(colTotal-1).length;
-
-                    StringBuilder sb;
-                    for(int row=rowIndexStart;row<rowTotal;row++) {
-                        sb = new StringBuilder();
-                        for(int col=0;col<colTotal;col++) {
-                            String contents = sheet.getCell(col, row).getContents();
-                            sb.append("col"+col+" : "+contents+" , ");
-                        }
-                        Log.i("test", sb.toString()); // 가져오는지 로그 확인
-                    }
-                }
-            }
-
-            if(wb2 != null) {
-                Sheet sheet = wb2.getSheet(0);   // 시트 불러오기
-                if(sheet != null) {
-                    int colTotal = sheet.getColumns();    // 전체 컬럼
-                    int rowIndexStart = 1;                  // row 인덱스 시작
-                    int rowTotal = sheet.getColumn(colTotal-1).length;
-
-                    StringBuilder sb2;
-                    for(int row=rowIndexStart;row<rowTotal;row++) {
-                        sb2 = new StringBuilder();
-                        for(int col=0;col<colTotal;col++) {
-                            String contents = sheet.getCell(col, row).getContents();
-                            sb2.append("col"+col+" : "+contents+" , ");
-                            if(col==12){
-                                teacher_num[row-1]=Integer.parseInt(contents);
-                                sb2.append("teacher_num[row] = "+teacher_num[row]+"  row = "+row+" success ");
-                            }else if(col==14){
-                                child_num[row-1]=Integer.parseInt(contents);
-                                sb2.append("child_num[row] = "+child_num[row]+"  row = "+row+" success ");
-                            }
-                        }
-                        Log.i("test", sb2.toString()); // 가져오는지 로그 확인
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (BiffException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
 
